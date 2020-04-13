1b2888992a50875cdce983c3618a65cb485ebb30
BokunoMasayume <allenwalker233333@gmail.com>
2020-04-13 19:56:22 +0800

    still only rotate and translate done:(

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const m3 = require('./lib/m3');
 
-let mat = m3.rotate(12);
+let mat = m3.scale(12);
 
-mat = m3.multipy(mat, m3.inverse.rotate(12));
-
+mat = m3.vMulMat([1,0,1], m3.inverse.translate(100,10));
+// mat = m3.mMulVec( m3.inverse.translate(10,20) , [100,0,1]);
 console.log(mat);
